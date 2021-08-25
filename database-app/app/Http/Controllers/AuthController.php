ca731090d6ba30b5ead2b366d0f1f75cd5cf3667
satrio wicaksono <iam.satriorahman28@gmail.com>
2021-08-25 21:08:32 +0700

    Test Controller Login

--- database-app/app/Http/Controllers/AuthController.php
+++ database-app/app/Http/Controllers/AuthController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function getLogin()
+    {
+        return view('login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        dd('Login berhasil');
+    }
 }
