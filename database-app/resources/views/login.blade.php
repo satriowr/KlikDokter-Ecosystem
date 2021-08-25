a8fe65e5cce8378d5c1080629b755ba317e9001c
satrio wicaksono <iam.satriorahman28@gmail.com>
2021-08-25 21:10:19 +0700

    Initial Commit

--- database-app/resources/views/login.blade.php
+++ database-app/resources/views/login.blade.php
@@ -12,22 +12,22 @@
 
     <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
     <link href="style.css" rel="stylesheet">
   </head>
-
+    
   <body class="text-center">
-    <form class="form-signin">
-        {{ csrf_field() }}
+    <form class="form-signin" method="POST" action="{{ route('login') }}">
+      {{ csrf_field() }}
       <img class="mb-4" src="https://i.im.ge/2021/08/25/BUPzJ.png" alt="" width="300" height="50">
       <br>
       <br>
       <br>
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
       <br>
       <button class="btn btn-lg btn-primary btn-block" style="background-color:#61A745; border-color:#61A745" type="submit">Sign in</button>
 
