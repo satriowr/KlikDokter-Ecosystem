8f4e2e8154b149e66ee27aa6e23d87796355b3ae
satrio wicaksono <iam.satriorahman28@gmail.com>
2021-08-30 19:53:10 +0700

    Testing Send Message

--- database-app/resources/views/broadcast.blade.php
+++ database-app/resources/views/broadcast.blade.php
@@ -40,7 +40,29 @@
     </div>
   </nav>
 
-  
+	<div class="send">
+		<h1>Testing Broadcast</h1>
+		<form class="testing" method="POST">
+			{{ csrf_field() }}
+			<b>No Tujuan</b><br>
+			<input type="text" name="nomor" placeholder="62" required maxlength="14">
+			<!-- <textarea class="form-control" required="" rows="10" spellcheck="false" name="pesan" id="pesan" placeholder="Format:
+Country code + Mobile digit + Phone number
+
+Example:
+62812345678
+62812345678901
+62812345678
+62812345678901"></textarea> -->
+			<br>
+
+			<b>Pesan</b>
+			<br>
+			<input type="text" name="pesan" placeholder="Tuliskan Pesan" required maxlength="160" >
+			<br><br>
+			<input type="submit" value="Kirim">
+		</form>
+	</div>
 
   <a>
     <h1 class="h3 mt-5 mb-3 font-weight-normal">
@@ -52,7 +74,11 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script>
+	
+</script>
 </html>
+
 
 
 
