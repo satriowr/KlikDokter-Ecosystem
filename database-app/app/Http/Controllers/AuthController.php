38ea18c390cd2fb637e81324cf16e9ea55a5e74c
satrio wicaksono <iam.satriorahman28@gmail.com>
2021-08-30 19:53:34 +0700

    Add Whatsapp API

--- database-app/app/Http/Controllers/AuthController.php
+++ database-app/app/Http/Controllers/AuthController.php
@@ -42,8 +42,25 @@ class AuthController extends Controller
         return redirect('/komunitas');
     }
 
-    public function broadcast(Request $request)
+    public function postBroadcast(Request $request)
     {
+		error_reporting(0);
+		$my_apikey = "LTI99GBWNE4JMDP9SWB1";
+		$destination = $_POST['nomor'];
+		$message = $_POST['pesan'];
+		$api_url = "http://panel.rapiwha.com/send_message.php";
+		$api_url .= "?apikey=". urlencode ($my_apikey);
+		$api_url .= "&number=". urlencode ($destination);
+		$api_url .= "&text=". urlencode ($message);
+		$api_url .= "?apikey=". urlencode ($my_apikey);
+		$api_url .= "&number=". urlencode ($destination);
+		$api_url .= "&text=". urlencode ($message);
+		$my_result_object = json_decode(file_get_contents($api_url, false));
+		echo "<br>Result: ". $my_result_object->success;
+		echo "<br>Description: ". $my_result_object->description;
+		echo "<br>Code: ". $my_result_object->result_code;
+	
         return redirect('/broadcast');
     }
+
 }
