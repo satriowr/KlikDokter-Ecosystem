91bfcccdbb12582402b8e81f01190e732aadb030
satrio wicaksono <iam.satriorahman28@gmail.com>
2021-08-31 21:26:27 +0700

    Fixed Bug

--- database-app/app/Http/Controllers/AuthController.php
+++ database-app/app/Http/Controllers/AuthController.php
@@ -43,23 +43,28 @@ class AuthController extends Controller
     }
 
     public function postBroadcast(Request $request)
-    {
-		error_reporting(0);
-		$my_apikey = "LTI99GBWNE4JMDP9SWB1";
-		$destination = $_POST['nomor'];
-		$message = $_POST['pesan'];
-		$api_url = "http://panel.rapiwha.com/send_message.php";
-		$api_url .= "?apikey=". urlencode ($my_apikey);
-		$api_url .= "&number=". urlencode ($destination);
-		$api_url .= "&text=". urlencode ($message);
-		$api_url .= "?apikey=". urlencode ($my_apikey);
-		$api_url .= "&number=". urlencode ($destination);
-		$api_url .= "&text=". urlencode ($message);
-		$my_result_object = json_decode(file_get_contents($api_url, false));
-		echo "<br>Result: ". $my_result_object->success;
-		echo "<br>Description: ". $my_result_object->description;
-		echo "<br>Code: ". $my_result_object->result_code;
-	
+    { 
+        $array = explode(PHP_EOL, $_POST['nomor']);
+        $total = count($array);
+        //dd($total);
+        for ($i=0; $i < $total ; $i++) {
+            //dd($total);
+            error_reporting(0);
+            $my_apikey = "AWP7MLKH3BPG06UQ6SOM";
+            $destination = $array[$i];
+            $message = $_POST['pesan'];
+            $api_url = "http://panel.rapiwha.com/send_message.php";
+            $api_url .= "?apikey=". urlencode ($my_apikey);
+            $api_url .= "&number=". urlencode ($destination);
+            $api_url .= "&text=". urlencode ($message);
+            $api_url .= "?apikey=". urlencode ($my_apikey);
+            $api_url .= "&number=". urlencode ($destination);
+            $api_url .= "&text=". urlencode ($message);
+            $my_result_object = json_decode(file_get_contents($api_url, false));
+            echo "<br>Result: ". $my_result_object->success;
+            echo "<br>Description: ". $my_result_object->description;
+
+        }
         return redirect('/broadcast');
     }
 
